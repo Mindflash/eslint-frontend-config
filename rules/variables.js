8b854ca738492c7ec64daceb8132909007ad2487
Wesley Cho <wesley.cho@gmail.com>
2017-02-27 21:38:53 -0800

    Add variable rules

---
+++ rules/variables.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  'rules': {
+    // enforce consistent variable declarations
+    'init-declarations': 0,
+
+    // prevent shadow variables inside of catch
+    // 'no-catch-shadow': 2,
+
+    // prevent use of the delete operator on variables
+    // 'no-delete-var': 2,
+
+    // prevent labels being given the same name as variables
+    // 'no-label-var': 2,
+
+    // restrict use of certain global variable names
+    // 'no-restricted-globals': 0,
+
+    // prevent shadowing of restricted names
+    // 'no-shadow-restricted-names': 2,
+
+    // prevent variable declarations from shadowing variables declared in outer scope
+    // 'no-shadow': 2,
+
+    // prevent initializing to undefined
+    // 'no-undef-init': 1,
+
+    // prevent use of undeclared variables unless mentioned in a /* global */ comment
+    'no-undef': 0,
+
+    // prevent use of undefined variable
+    'no-undefined': 1,
+
+    // prohibit unused variable
+    'no-unused-vars': 0,
+
+    // prohibit variable and function use before their definition
+    // 'no-use-before-define': 2
+  }
+}
+
